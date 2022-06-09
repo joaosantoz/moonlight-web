24d34b7e6fe4cf07739b0f9951c5640aa5ab6a49
joaosantoz <jvsantosdev@gmail.com>
2022-06-09 18:36:25 -0300

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  globals: {
+    defineEmits: true,
+    document: true,
+    localStorage: true,
+    GLOBAL_VAR: true,
+    window: true,
+    defineProps: true,
+    defineExpose: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-strongly-recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint', 'import'],
+  rules: {
+    'no-console': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+  },
+};
